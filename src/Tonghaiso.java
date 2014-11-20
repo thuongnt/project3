a990228a9bd51927bee18c07f38f9394dfe7ae26
KhiemNS <khiemns.k54@gmail.com>
2014-11-20 20:41:56 +0700

    commit1

---
+++ src/Tonghaiso.java
@@ -0,0 +1,13 @@
+
+public class Tonghaiso {
+	public static void main(String[] aa){
+		int a=1;
+		int b=2;
+		System.out.println(tongHaiSo(a, b));
+	}
+	public static int tongHaiSo(int a, int b){
+		int m;
+		m=a+b;
+		return m;
+	}
+}
