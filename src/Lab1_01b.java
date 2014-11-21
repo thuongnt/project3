86fa17bd662de9e41ab30e8a702d437b30c97a81
KhiemNS <thuongnguyen.dhhn@gmail.com>
2014-11-21 22:30:37 +0700

    lab01_01& 02

---
+++ src/Lab1_01b.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Lab1_01b {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int number1;
+		int number2;
+		int number3;
+
+		System.out.print("Enter first integer: ");
+		number1 = input.nextInt();
+
+		System.out.print("Enter second integer: ");
+		number2 = input.nextInt();
+
+		System.out.print("Enter 3rd integer: ");
+		number3 = input.nextInt();
+		
+		System.out.println(sum(number1,number2));
+		System.out.println(product(number1,number2));
+		System.out.println(difference(number1,number2));
+		System.out.println(quotient(number1,number2));
+		System.out.println(smallest(number1,number2,number3));
+		System.out.println(largest(number1,number2,number3));
+
+	}
+
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+
+	public static int difference(int a, int b) {
+		return a - b;
+	}
+
+	public static int product(int a, int b) {
+		return b * a;
+	}
+
+	public static int quotient(int a, int b) {
+		if (b == 0) {
+			System.out.println("Gia tri khong hop ly!!!");
+			return 0;
+		} else
+			return a / b;
+	}
+
+	public static int smallest(int a, int b, int c) {
+//		if (a > b) {
+//			a = b;
+//		}
+//		if (a > c)
+//			return c;
+//		return a;
+		a = a < b ? a : b;
+		return a < c ? a : c;
+	}
+	
+	public static int largest(int a, int b, int c) {
+		a = a > b ? a : b;
+		return a > c ? a : c;
+	}
+	
+}
